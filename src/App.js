9e2947ad3dbe90334f5f1114d0091dee758d6cbf
Lance Cruzado <raphaellcruzado@gmail.com>
2023-08-08 19:04:48 -0700

    adding components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Navbar from '../src/components/Navbar';
+import Button from './components/Button';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <h1>love</h1>
+      <Navbar />
+      <Button />
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Button.css'
+
+function Button() {
+    return (
+        <div className="button">
+        </div>
+    );
+}
+
+export default Button;
