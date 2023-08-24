edb82c534b8a0bd057137adfb46f3ac648d52241
Lance Cruzado <raphaellcruzado@gmail.com>
2023-08-23 19:42:47 -0700

    setting up main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Navbar from '../src/components/Navbar';
 import Button from './components/Button';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      
-      <Button />
-    </div>
+    <>
+    <Navbar />
+      {/* <Route path={Navbar} /> */}
+    </>
+   
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
     <>
-        <nav className="navbar">
-            <div className="navbar-container">
-                <Link to='/' className='navbar-logo'>
-                    TRVL <i className="fac fa-typo3" />
-                </Link>
-            </div>
-        </nav>
+    <h1>yo</h1>
+
+    <nav className="navbar">
+        <div className="navbar-container">
+            {/* <Link to='/' className="navbar-logo">
+                TRVL <i className="fac fa-typo3" />
+            </Link> */}
+        </div>
+    </nav>
     </>
         
     );
