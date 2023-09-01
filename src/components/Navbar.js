8933722a93cdc12afa9c2809f633455b2106cc01
Lance Cruzado <raphaellcruzado@gmail.com>
2023-08-31 22:50:32 -0700

    nav item links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faShieldHalved, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faShieldHalved, faTimes, navMenu } from '@fortawesome/free-solid-svg-icons'
 
 
 function Navbar() {
     const [click, setClick] = useState(false); 
 
-    const handleClick = () => setClick(!click);
+    const handleClick = () => {
+        setClick(!click);
+    };
+    const closeMobileMenu = () => setClick(false);
 
     return (
     <>
@@ -19,6 +22,28 @@ function Navbar() {
                 <div className="menu-icon" onClick={handleClick}>
                     <FontAwesomeIcon icon= {click ? faTimes : faBars} />
                 </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className="nav-item">
+                        <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/services' className="nav-links" onClick={closeMobileMenu}>
+                            Services
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/products' className="nav-links" onClick={closeMobileMenu}>
+                            Products
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/sign-up' className="nav-links-mobile" onClick={closeMobileMenu}>
+                            Sign Up
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     </>
