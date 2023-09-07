dcfb2af3dc16303431524a307b0189a4963846d3
Lance Cruzado <raphaellcruzado@gmail.com>
2023-09-07 11:28:01 -0700

    finalizing button component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import Navbar from '../src/components/Navbar';
 import {
+  Routes,
   Route
 } from "react-router-dom";
-// import './App.css'
+import './App.css';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/products' component={Products} />
+        <Route path='/sign-up' component={SignUp} />
+      </Routes>
     </>
   )
   
