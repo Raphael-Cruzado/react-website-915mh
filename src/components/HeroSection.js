4cab81b80b46927e538a21114f1b7dcc3f629346
Lance Cruzado <raphaellcruzado@gmail.com>
2023-09-13 23:40:42 -0700

    still video

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,26 +3,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
 import { Button } from './Button';
 import './HeroSection.css';
+import myVideo from '../videos/video-2.mp4';
 
 function HeroSection() {
 
-  const myVideo = document.getElementById('videoId');
-  console.log (myVideo.type);
+  const theVideo = document.getElementById('videoId');
+  console.log(theVideo);
+
   return (
     <div className='hero-container'>
-        <video id='videoId' src='videos/video-2.mp4' type='video/mp4' autoPlay loop muted />
-        <h1>ADVENTURE AWAITS</h1>
-        <p>What are you waiting for?</p>
-        <div className='hero-btns'>
-            <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
-                GET STARTED
-            </Button>
-            <Button className='btns' buttonStyle='btn-primary' buttonSize='btn--large'>
-                WATCH TRAILER <FontAwesomeIcon icon={faPlayCircle} />
-            </Button>
-        </div>
+      <video id='videoId' src={myVideo} autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      <div className='hero-btns'>
+          <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
+              GET STARTED
+          </Button>
+          <Button className='btns' buttonStyle='btn-primary' buttonSize='btn--large'>
+              WATCH TRAILER <FontAwesomeIcon icon={faPlayCircle} />
+          </Button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default HeroSection;
